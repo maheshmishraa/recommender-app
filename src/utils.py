dfd763adf3dba723119df4f3a6b6502849562821
Mahesh Mishra <maheshmishradata@gmail.com>
2026-04-05 15:50:25 +0530

    Fix: Create models directory automatically when saving pickle files

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,12 @@
 import pickle
+import os
 
 def load_pickle(path):
     return pickle.load(open(path, "rb"))
 
 def save_pickle(obj, path):
+    # Create directory if it doesn't exist
+    os.makedirs(os.path.dirname(path), exist_ok=True)
     pickle.dump(obj, open(path, "wb"))
 
 def load_model(tfidf_path, similarity_path):
